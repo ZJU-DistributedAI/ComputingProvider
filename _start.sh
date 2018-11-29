9039dcb07ce5de9faecc4c2dd2a014c78b219ec4
fallenk <fallenk_liu@163.com>
2018-11-29 21:33:17 +0800

    init computing provider client

---
+++ _start.sh
@@ -0,0 +1,2 @@
+docker-compose run --rm start_dependencies
+docker-compose up
---
+++ main.go
@@ -0,0 +1,40 @@
+//go:generate goagen bootstrap -d github.com/ZJU-DistributedAI/Common/design
+//go:generate goagen -d github.com/ZJU-DistributedAI/Common/design swagger -o public
+//go:generate goagen -d github.com/ZJU-DistributedAI/Common/design schema -o public
+//go:generate go-bindata -ignore 'bindata.go' -pkg swagger -o public/swagger/bindata.go ./public/swagger/...
+//go:generate echo Start copying the swagger-ui dist resources
+//go:generate cp -a swagger-ui-dist/. public/swagger
+//go:generate echo Finished copying the swagger-ui dist resources
+
+package main
+
+import (
+	"github.com/ZJU-DistributedAI/Common/app"
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/middleware"
+)
+
+func main() {
+	// Create service
+	service := goa.New("Computing Providers")
+
+	// Mount middleware
+	service.Use(middleware.RequestID())
+	service.Use(middleware.LogRequest(true))
+	service.Use(middleware.ErrorHandler(service, true))
+	service.Use(middleware.Recover())
+
+	// Mount storage controllers
+	storageController := NewStorageController(service)
+	app.MountStorageController(service, storageController)
+
+	// Mount public controlllers
+	publicController := NewPublicController(service)
+	app.MountPublicController(service, publicController)
+
+	// Start service
+	if err := service.ListenAndServe(":3001"); err != nil {
+		service.LogError("startup", "err", err)
+	}
+
+}
