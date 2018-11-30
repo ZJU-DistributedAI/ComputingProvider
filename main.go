663a03d4ec0543609095fdc84a621b5c86786b7e
fallenk <fallenk_liu@163.com>
2018-12-01 01:01:57 +0800

    init .sh file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 //go:generate goagen bootstrap -d github.com/ZJU-DistributedAI/ComputingProvider/design
 //go:generate goagen -d github.com/ZJU-DistributedAI/ComputingProvider/design swagger -o public
 //go:generate goagen -d github.com/ZJU-DistributedAI/ComputingProvider/design schema -o public
+
 //go:generate go-bindata -ignore 'bindata.go' -pkg swagger -o public/swagger/bindata.go ./public/swagger/...
 //go:generate echo Start copying the swagger-ui dist resources
 //go:generate cp -a swagger-ui-dist/. public/swagger
@@ -16,7 +17,7 @@ import (
 
 func main() {
 	// Create service
-	service := goa.New("Computing Providers")
+	service := goa.New("computingProvider service APIs")
 
 	// Mount middleware
 	service.Use(middleware.RequestID())
@@ -24,13 +25,12 @@ func main() {
 	service.Use(middleware.ErrorHandler(service, true))
 	service.Use(middleware.Recover())
 
-	// Mount storage controllers
-	storageController := NewStorageController(service)
-	app.MountStorageController(service, storageController)
-
-	// Mount public controlllers
-	publicController := NewPublicController(service)
-	app.MountPublicController(service, publicController)
+	// Mount "Public" controller
+	c := NewPublicController(service)
+	app.MountPublicController(service, c)
+	// Mount "Storage" controller
+	c2 := NewStorageController(service)
+	app.MountStorageController(service, c2)
 
 	// Start service
 	if err := service.ListenAndServe(":3001"); err != nil {
--- storage.go
+++ storage.go
@@ -1,87 +1,36 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"github.com/ZJU-DistributedAI/ComputingProvider/app"
 	"github.com/goadesign/goa"
-	"io"
-	"io/ioutil"
-	"mime/multipart"
-	"net/http"
-	"os"
 )
 
-var IPFS_HOST = os.Getenv("IPFS_HOST")
-
-// StorageController implements the storage resource.
+// StorageController implements the Storage resource.
 type StorageController struct {
 	*goa.Controller
 }
 
-// NewStorageController creates a storage controller.
+// NewStorageController creates a Storage controller.
 func NewStorageController(service *goa.Service) *StorageController {
 	return &StorageController{Controller: service.NewController("StorageController")}
 }
 
-func (c *StorageController) Cat(ctx *app.CatStorageContext) error {
-	var url = fmt.Sprintf("http://%s:5001/api/v0/cat?arg=%s", IPFS_HOST, ctx.Address)
-	goa.LogInfo(ctx, fmt.Sprintf("Calling IPFS Cat API, at url: %s", url))
-	resp, err := http.Get(url)
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Failed calling IPFS", "API", "cat", "Err", err.Error()))
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Failed reading IPFS response", "API", "cat", "Err", err.Error()))
-	}
-	return ctx.OK([]byte(body))
+// Add runs the add action.
+func (c *StorageController) Add(ctx *app.AddStorageContext) error {
+	// StorageController_Add: start_implement
+
+	// Put your logic here
+
+	return nil
+	// StorageController_Add: end_implement
 }
 
-func (c *StorageController) Add(ctx *app.AddStorageContext) error {
-	file, err := ctx.Payload.File.Open()
-	if err != nil {
-		return ctx.BadRequest(
-			goa.ErrBadRequest("Could not open file", "API", "add", "Err", err.Error()))
-	}
-	defer file.Close()
+// Cat runs the cat action.
+func (c *StorageController) Cat(ctx *app.CatStorageContext) error {
+	// StorageController_Cat: start_implement
 
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile("", "")
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Could not create form file", "API", "add", "Err", err.Error()))
-	}
-	_, err = io.Copy(part, file)
-	err = writer.Close()
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Could not close form writter", "API", "add", "Err", err.Error()))
-	}
+	// Put your logic here
 
-	url := fmt.Sprintf("http://%s:5001/api/v0/add", IPFS_HOST)
-	req, err := http.NewRequest("POST", url, body)
-	req.Header.Set("Content-Type", writer.FormDataContentType())
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Error creating post request", "API", "add", "Err", err.Error()))
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Error posting request to IPFS", "API", "add", "Err", err.Error()))
-	}
-	defer resp.Body.Close()
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return ctx.InternalServerError(
-			goa.ErrInternal("Error closing IPFS response body", "API", "add", "Err", err.Error()))
-	}
-	return ctx.OK([]byte(responseBody))
+	return nil
+	// StorageController_Cat: end_implement
 }
